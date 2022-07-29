0a456d107f0ae83d38c99f0886f9bed1a79b3a7a
sami-theion <sami@theion.de>
2022-07-29 13:39:23 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,12 @@
-#include "Windows.h"
+// A C++ program that compiles and runs another C++
+// program
+#include "iostream"
+using namespace std;
+int main ()
+{
+
+	cout << "Running file ";
+	system("C:/Users/SamiDhiab/Theion_Repos/soft_wafer_holder_system_controller/dependencies/lib_keyence_distance_sensor/build/keyence_bin.exe");
+
+	return 0;
+}
