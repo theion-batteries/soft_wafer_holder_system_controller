45021b42c416ce33fabf6c6a4ad6591c99e2054b
sami-theion <sami@theion.de>
2022-08-01 10:13:33 +0200

    added files

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 // A C++ program that compiles and runs another C++
 // program
+#include "Windows.h"
 #include "iostream"
-using namespace std;
+
 int main ()
 {
-
-	cout << "Running file ";
-	system("C:/Users/SamiDhiab/Theion_Repos/soft_wafer_holder_system_controller/dependencies/lib_keyence_distance_sensor/build/keyence_bin.exe");
-
+	std::string pyFile ="repetier_manager.py";
+	std::string pyFilePath = "C:/Users/SamiDhiab/Theion_Repos/soft_wafer_holder_system_controller/dependencies/software_repetier_rest_api/src/repetier_manager_lib/";
+	std::string pyCmd = "python ";
+	//cout << "Running keyence program ";
+	//system("C:/Users/SamiDhiab/Theion_Repos/soft_wafer_holder_system_controller/dependencies/lib_keyence_distance_sensor/build/keyence_bin.exe");
+	std::cout << "Running delta program ";
+	// shell execute 
+	ShellExecute(hwnd, "find", "c:\\MyPrograms", NULL, NULL, 0);
 	return 0;
 }
