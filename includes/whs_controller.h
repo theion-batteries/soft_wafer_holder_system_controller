8f20094a7b4e46d6e357d1093b2449e861cc1c9d
sami-theion <sami@theion.de>
2022-08-01 14:34:25 +0200

    updated sub

---
+++ includes/whs_controller.h
@@ -0,0 +1,28 @@
+/**
+   @file whs_controller.h
+   @author sami dhiab (sami@theion.de)
+   @brief controller wafer holder system
+   @version 0.1
+   @date 2022-08-01
+
+   @copyright Copyright (c) 2022
+
+*/
+#pragma once
+
+class whs_controller
+{
+private:
+    /* data */
+public:
+    whs_controller(/* args */);
+    ~whs_controller();
+};
+
+whs_controller::whs_controller(/* args */)
+{
+}
+
+whs_controller::~whs_controller()
+{
+}
