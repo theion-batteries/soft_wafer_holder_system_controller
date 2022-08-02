b1f3dff6011e386eb6891c164cb7d3883edb6e91
sami-theion <sami@theion.de>
2022-08-02 12:49:49 +0200

    added 2 tests server

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,24 @@
 // program
 
 #include "whs_controller.h"
-void debug();
+void run_test_server();
 int main ()
 {
 
-
+	//run_test_server();
 	whs_controller wafer_sys_control;
-	wafer_sys_control.run_keyence_subprocess();
-	wafer_sys_control.run_delta_subprocess();
+	//wafer_sys_control.run_keyence_subprocess();
+	wafer_sys_control.connect_to_delta_server();
+	//wafer_sys_control.run_delta_subprocess();
+	wafer_sys_control.connect_to_keyence_server();
 
 	return 0;
 }
 
-void debug()
+void run_test_server()
 {
-	LPCWSTR pyFilePath = L"C:/Users/SamiDhiab/Theion_Repos/soft_wafer_holder_system_controller/dependencies/software_repetier_rest_api/src/repetier_manager_lib/command_parser.py";
+	LPCWSTR pyFilePath = L"C:/Users/SamiDhiab/Theion_Repos/soft_wafer_holder_system_controller/tests/server.py";
 	LPCWSTR pyCmd = L"C:/Users/SamiDhiab/AppData/Local/Programs/Python/Python39/python.exe";
-	LPCWSTR cppFile =L"C:/Users/SamiDhiab/Theion_Repos/soft_wafer_holder_system_controller/dependencies/lib_keyence_distance_sensor/build/Debug/keyence_bin.exe";
-   	   std::cout << "Running delta program " <<std::endl;
-
+   	std::cout << "Running test server program " <<std::endl;
 	ShellExecuteW(NULL, L"open", pyCmd, pyFilePath, NULL, SW_SHOWDEFAULT);
-       std::cout << "Running keyence program " <<std::endl;
-
-	ShellExecuteW(NULL, L"open", cppFile, NULL, NULL, SW_SHOWDEFAULT); 
 }
