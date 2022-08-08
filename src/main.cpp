a93f8c90e5c7f49189ca2470a7e31ef513a0a342
sami-theion <sami@theion.de>
2022-08-08 14:17:26 +0200

    updated subs

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,15 @@
 int main ()
 {
 	whs_controller wafer_sys_control;
+	// keyence
 	wafer_sys_control.run_keyence_subprocess(); // to run subprocesses
-	wafer_sys_control.run_delta_subprocess();
 	wafer_sys_control.connect_to_keyence_server(); // connect to test server
 	wafer_sys_control.set_keyence_mesurement_mode(); // 
-	wafer_sys_control.get_keyence_sensor_mesured_Values();
-
+	//wafer_sys_control.get_keyence_sensor_mesured_Values();
+	// delta
+	wafer_sys_control.run_delta_subprocess();
 	wafer_sys_control.connect_to_delta_server();
 	wafer_sys_control.get_delta_position();
-	wafer_sys_control.close_all_sockets();
+	//wafer_sys_control.close_all_sockets();
 	return 0;
 }
